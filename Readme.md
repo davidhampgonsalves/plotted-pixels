2412f42eeabbb80600c825be27c37d08cb7f9e7f
David Hamp-Gonsalves <davidhampgonsalves@gmail.com>
2022-10-24 07:50:19 -0600

    wrap everything up and update readme

---
+++ Readme.md
@@ -0,0 +1,14 @@
+<img src="https://github.com/davidhampgonsalves/plotted-pixels/raw/master/header.jpg" width="800"/>
+
+Plotted Pixels is a small cli written in go-lang for turning pixel art into vector based images that are easily plottable. Its aim is to maximize line lengths to avoid pen up / down motions. 
+
+## Usage
+Clone repo and then `go run main.go file.png`
+
+## About
+
+This was a small afternoon project to play around with some ideas I had to avoid the full complexity of the traveling sales man problem (which this task falls into if you want to generate optimal paths) and see what kind of results I could achieve with some fast snake like approaches. 
+
+I continued working on general vector fills for pen plotters in my subsiquent (https://github.com/davidhampgonsalves/restaline)[Restaline] project.
+
+<img align="right" src="https://github.com/davidhampgonsalves/plotted-pixels/raw/master/examples/link.png" width="300"/>
--- main_test.go
+++ main_test.go
@@ -1,78 +1,80 @@
 package main
 
 import (
-  "testing"
+	"testing"
 
 	"image/color"
 )
 
 func Equal(a, b []int) bool {
-    if len(a) != len(b) {
-        return false
-    }
-    return true
+	if len(a) != len(b) {
+		return false
+	}
+	return true
 }
 
 func ComparePositions(a, b [][]int, t *testing.T) {
-  if(len(a) != len(b)) {
-    t.Errorf("position expected / actual lengths are different: %v vs. %v.", a, b)
-  }
+	if len(a) != len(b) {
+		t.Errorf("position expected / actual lengths are different: %v vs. %v.", a, b)
+	}
 
-  for i := range a {
-    for j := range a[0] {
-      if a[i][j] != b[i][j] {
-        t.Errorf("%v - actual: %v does not equal expected: %v.", i, a, b)
-      }
-    }
-  }
+	for i := range a {
+		for j := range a[0] {
+			if a[i][j] != b[i][j] {
+				t.Errorf("%v - actual: %v does not equal expected: %v.", i, a, b)
+			}
+		}
+	}
 }
 
 //func TestGetStartPositions(t *testing.T) {
-  //bitmask := [][]bool {
-    //{true, false, true, false},
-    //{false, true, false, false},
-    //{false, false, true, false},
-    //{false, false, true, true},
-  //}
+//bitmask := [][]bool {
+//{true, false, true, false},
+//{false, true, false, false},
+//{false, false, true, false},
+//{false, false, true, true},
+//}
 
-  //startPositions := GetStartPositions(0, 0, bitmask, CloneBitMask(bitmask))
-  //expectedPositions := [][]int {
-    //{0, 2},
-    //{3, 3},
-    //{3, 2},
-    //{0, 0},
-  //}
+//startPositions := GetStartPositions(0, 0, bitmask, CloneBitMask(bitmask))
+//expectedPositions := [][]int {
+//{0, 2},
+//{3, 3},
+//{3, 2},
+//{0, 0},
+//}
 
-  //ComparePositions(startPositions, expectedPositions, t)
+//ComparePositions(startPositions, expectedPositions, t)
 //}
 
 //func TestSimpleGetStartPositions(t *testing.T) {
-  //bitmask := [][]bool {
-    //{true, false},
-    //{false, true},
-  //}
+//bitmask := [][]bool {
+//{true, false},
+//{false, true},
+//}
 
-  //startPositions := GetStartPositions(0, 0, bitmask, CloneBitMask(bitmask))
-  //expectedPositions := [][]int {
-    //{1, 1},
-    //{0, 0},
-  //}
+//startPositions := GetStartPositions(0, 0, bitmask, CloneBitMask(bitmask))
+//expectedPositions := [][]int {
+//{1, 1},
+//{0, 0},
+//}
 
-  //ComparePositions(startPositions, expectedPositions, t)
+//ComparePositions(startPositions, expectedPositions, t)
 //}
 
 func TestLoadAndStartPositions(t *testing.T) {
-  img := Load("test.4x4.png")
+	img := Load("test.4x4.png")
 
-  bitmask := GenerateBitmask(img, color.NRGBA{0x00, 0x80, 0xf8, 0xff})
+	bitmask := GenerateBitmask(img, color.NRGBA{0x00, 0x80, 0xf8, 0xff})
 
-  startPositions := GetStartPositions(3, 0, bitmask, CloneBitMask(bitmask))
+	startPositions := GetStartPositions(3, 0, bitmask, CloneBitMask(bitmask))
 
-  expectedPositions := [][]int {
-    {0, 1},
-    {3, 2},
-    {2, 3},
-    {3, 0},
-  }
-  ComparePositions(startPositions, expectedPositions, t)
+	expectedPositions := [][]int{
+		{0, 1},
+		{3, 2},
+		{2, 3},
+		{1, 2},
+		{2, 1},
+		{3, 0},
+	}
+	ComparePositions(startPositions, expectedPositions, t)
 }
